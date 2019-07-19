1893d79366c86c13082d6c6b6b71730bdfb4d4fd
adionisiodr <adionisio82@gmail.com>
2019-07-19 00:48:27 -0400

    implemeting a simple greet server and client

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 
-	"github.com/simple/grpc-go-course/greet/greetpb"
+	"github.com/simple/golang_api_microservice/greet/greetpb"
 	"google.golang.org/grpc"
 )
 
@@ -21,5 +22,28 @@ func main() {
 
 	// create client
 	c := greetpb.NewGreetServiceClient(cc)
-	fmt.Printf("Created client: %f", c)
+	// fmt.Printf("Created client: %f", c)
+
+	doUnary(c)
+
+}
+
+// doUnary executes a GreetRequest and logs results
+func doUnary(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting to do Unary RPC...")
+	// GreetRequest's struct has a pointer to a Greeting, which contains a FirstName and LastName
+	req := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Angel",
+			LastName:  "Dionisio",
+		},
+	}
+
+	res, err := c.Greet(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling Greet RPC: %v", err)
+	}
+
+	log.Printf("Response from Greet: %v", res.Result)
+	// c.Greet(context.Background(), in *GreetRequest, opts ...grpc.CallOption) (*GreetResponse, error)
 }
--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 
-	"github.com/simple/grpc-go-course/greet/greetpb"
+	"github.com/simple/golang_api_microservice/greet/greetpb"
 	"google.golang.org/grpc"
 )
 
 type server struct{}
+
+// Greet implements the `GreetServiceServer` interface in greet.pb.go (greet protobuff)
+// this defines our server, takes context, a pointer to GreetRequest and returns
+// a pointer to GreetResponse and an error
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	fmt.Printf("Greet function was invoked with %v\n", req)
+	// we can get GetGreeting because request (req) is a greetRequest and it contains a greeting
+	firstName := req.GetGreeting().GetFirstName()
+	result := "Hello " + firstName
+
+	// GreetResponse is a struct that returns key `Result` of type string
+	res := &greetpb.GreetResponse{
+		Result: result,
+	}
+
+	return res, nil
+}
 
 func main() {
 	fmt.Println("Hello from server!")
