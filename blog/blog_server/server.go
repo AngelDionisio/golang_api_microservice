765dd02e8272440c103a11e09ad6a8b8d0a132ef
Angel Dionisio <angeldionisio@angels-mbp-2.lan>
2019-08-18 22:47:40 -0400

    configuring mongodb

--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -1,23 +1,53 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/angel/golang_api_microservice/blog/blogpb"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 )
 
+var collection *mongo.Collection
+
 type server struct{}
+
+type blogItem struct {
+	ID       primitive.ObjectID `bson:"_id,omitEmpty"`
+	AuthorId string             `bson:"author_id"`
+	Content  string             `bson:"content"`
+	Title    string             `bson:"title"`
+}
 
 func main() {
 	// if we crash the go code, we get teh filename and line number
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
+	// configure MongoDb
+	// using username / password: "mongodb://foo:bar@localhost:27017"
+	fmt.Println("Connecting to MongoDb")
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("Blog Service Started")
+
+	collection = client.Database("mydb").Collection("blog")
 
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
@@ -47,5 +77,7 @@ func main() {
 	s.Stop()
 	fmt.Println("Closing the listener")
 	lis.Close()
+	fmt.Println("Closing MongoDB Connection")
+	client.Disconnect(ctx)
 	fmt.Println("End of program")
 }
