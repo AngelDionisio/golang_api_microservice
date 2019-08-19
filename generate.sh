1bf135a77178b815cf0ab395fadfa6f245b09f6e
Angel Dionisio <angeldionisio@angels-mbp-2.lan>
2019-08-18 20:49:46 -0400

    setting up scaffolding code and server for blog service

--- generate.sh
+++ generate.sh
@@ -2,3 +2,8 @@
 
 protoc greet/greetpb/greet.proto --go_out=plugins=grpc:.
 protoc calculator/calculatorpb/calculator.proto --go_out=plugins=grpc:.
+protoc blog/blogpb/blog.proto --go_out=plugins=grpc:.
+
+# to start mongodb database
+# from /Users/angeldionisio/Documents/mongodb-macos-x86_64-4.2.0
+# bin/mongod --dbpath data/db
